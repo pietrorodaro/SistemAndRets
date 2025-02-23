0ddcd8b31984e8d249ea26d28b2a6cb6bdc5172b
Pietro Rodaro <11pietro.rodaro@gmail.com>
2025-02-23 14:00:29 +0100

    Aggiunta file

---
+++ ConsoleAppClient/ConsoleAppClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class SocketClient
+{
+    public static void Main(string[] args)
+    {
+        StartClient();
+    }
+
+    public static void StartClient()
+    {
+        //Messaggio da mandare
+        byte[] buffer = new byte[1024];
+
+        try
+        {
+            //Esattamente stessa cosa di prima
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //Primitiva CLient a remoteEP che indirizzo loopback
+                sender.Connect(remoteEP);
+                //COnferma
+                Console.WriteLine("Connessione stabilita con {0}", sender.RemoteEndPoint.ToString());
+                //Messaggio da mandare con EOF alla fine
+                string message = "Ullallailolaiilolaiola<EOF>";
+                //Converte in bytr e lo manda
+                byte[] msgBytes = Encoding.ASCII.GetBytes(message);
+                //Primitiva dell'invio
+                sender.Send(msgBytes);
+                //NON LO SAPPIAMO CHATTTEE AIUTOOO
+                Console.WriteLine("Inviati {0} byte al server.", sender.RemoteEndPoint.ToString());
+                //Riceve echo server GAYY
+                int bytesRec = sender.Receive(buffer);
+                string response = Encoding.ASCII.GetString(buffer, 0, bytesRec);
+                Console.WriteLine("Ricevuto dal server: {0}", response);
+            }
+            catch (Exception e) 
+            { 
+                Console.WriteLine("ERRORE in fase di connsessione/trasferimento: " + e.Message);
+            }
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine("ERRORE generale: ", ex.Message);
+        }
+
+        Console.WriteLine("\n Premi un tasto per uscire...");
+        Console.ReadKey();
+
+    }
+}
+
+
---
+++ ConsoleAppServer/ConsoleAppServer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ConsoleAppServer
+{
+    public class SocketListener
+    {
+
+        public static int Main(String[] args)
+        {
+            StartServer();
+            return 0;
+        }
+
+        public static void StartServer()
+        {
+            //Prende la lista di tutti gli host del computer TRAMITE classe DNS che fa tutto sola
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            //Della lista prendiamo quello all'indice 1(loopback)
+            IPAddress ipAddress = host.AddressList[1];
+            //Associ indirizzo IP alla porta 11000
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+            try
+            {
+                //Prima sokcet con tutti i parametri indirizzo ip, tipo trasmissione (duplex), Tipo di protocollo
+                Socket Listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                //Primitva del server
+                Listener.Bind(localEndPoint);
+                //Massimo 10 processi in attessa
+                Listener.Listen(10);
+                //Messaggio attesa connessione
+                Console.WriteLine("Waiting for a connection . . .");
+                //Primitiva del sever
+                Socket handler = Listener.Accept();
+
+                string data = null;
+
+                byte[] bytes = null;
+
+                while (true)
+                {
+                    //Messagio del Client
+                    bytes = new byte[1024];
+                    //Primitiva prendi messaggio e lo ficchi lì bytesRec è la lunghezza
+                    int bytesRec = handler.Receive(bytes);
+                    //Metti in stringa il messaggio con la lunghezza del messaggio
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    //Se trova EOF chiude comunicazione
+                    if(data.IndexOf("<EOF>") > -1)
+                    {
+                        break;  
+                    }
+                }
+                //Conferma messaggio e stampa
+                Console.WriteLine("Text received : {0}", data);
+                //Ritrasforma messaggio in byte 
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                //Primitiva manda al Client torna uguale ECHO
+                handler.Send(msg);
+                //Chiude entrambe le socket
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+        
+    }
+}
+
+
